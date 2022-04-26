28352ac84b34965bee2c868163f12e980b3342ab
Lewis Staples <lewis.staples@gmail.com>
2022-04-26 19:41:03 -0400

    Day 03 Part A working

---
+++ day03/day03.py
@@ -0,0 +1,27 @@
+# adventOfCode 202- day ??
+# https://adventofcode.com/202-/day/??
+
+from fileinput import filename
+
+forest = set()
+current_location = [0,0]
+tree_tally = 0
+
+# Reading input from the input file
+input_filename='input_sample0.txt'
+with open(input_filename) as f:
+    # Pull in each line from the input file
+    for row_num, in_string in enumerate(f):
+        in_string = in_string.rstrip()
+        for col_num in [i for i,ch in enumerate(in_string) if ch=='#']:
+            forest.add( (row_num, col_num) )
+
+while current_location[0] <= row_num:
+    if tuple(current_location) in forest:
+        tree_tally += 1
+    current_location[0] += 1
+    current_location[1] = (current_location[1] + 3) % (len(in_string))
+
+print(f'Reading data in from input file: {input_filename}')
+print(f'The answer is: {tree_tally}')
+
