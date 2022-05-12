b7689fa7fde89aa9cd21910d6c5d3db0934e3291
Lewis Staples <lewis.staples@gmail.com>
2022-05-12 14:01:35 -0400

    Day 13 Part A working

--- day13/day13.py
+++ day13/day13.py
@@ -1,8 +1,11 @@
 # adventOfCode 2020 day 13
 # https://adventofcode.com/2020/day/13
 
-input_filename='input_sample0.txt'
-print(f'Using input file: {input_filename}')
+earliest_timestamp = None
+bus_IDs = None
+
+input_filename='input.txt'
+print(f'\nUsing input file: {input_filename}\n')
 with open(input_filename) as f:
     in_string1 = f.readline().rstrip()
     earliest_timestamp = int(in_string1)
@@ -13,5 +16,17 @@ with open(input_filename) as f:
     # get rid of variables that won't be needed anymore
     del in_string1, in_string2, string_list, f, input_filename
 
-    dummy = 123
-    
+earliest_known_bus = {
+    'bus_ID': -1,
+    'earliest_departure': float('inf')
+}
+for this_bus_ID in bus_IDs:
+    # print(this_bus_ID, end=', ')
+    # print(this_bus_ID - earliest_timestamp % this_bus_ID)
+    earliest_departure_for_this_bus = this_bus_ID - earliest_timestamp % this_bus_ID
+    if earliest_departure_for_this_bus < earliest_known_bus['earliest_departure']:
+        earliest_known_bus['bus_ID'] = this_bus_ID
+        earliest_known_bus['earliest_departure'] = earliest_departure_for_this_bus
+
+soln_A = earliest_known_bus['bus_ID'] * earliest_known_bus['earliest_departure']
+print(f'The solution to Part A is {soln_A}\n')
