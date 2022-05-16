e1601ccfc922f6cd8915521b89454a13a41e65f0
Lewis Staples <lewis.staples@gmail.com>
2022-05-15 21:43:23 -0400

    Day 13 Part B returning to list, but only filling a few entries and the extrapolating to solve it

--- day13/day13_partB.py
+++ day13/day13_partB.py
@@ -5,7 +5,7 @@ import copy
 
 bus_pair = []
 
-input_filename='input_sample5.txt'
+input_filename='input_sample0.txt'
 print(f'\nUsing input file: {input_filename}')
 with open(input_filename) as f:
     f.readline().rstrip()
@@ -19,33 +19,43 @@ with open(input_filename) as f:
     # get rid of variables that won't be needed anymore
     del in_string2, string_list, f, input_filename, busID
 
-time_combination, period_combination = bus_pair.pop()
-print(f'{time_combination} , {period_combination}')
+# sort bus_pair by element 1 in tuple (therefore period_new < period_combination)
+bus_pair.sort(key=lambda tup: tup[1])
+
+start_time_combination, period_combination = bus_pair.pop()
+print(f'{start_time_combination} , {period_combination}')
 
 while len(bus_pair) > 0:
     start_time_new, period_new = bus_pair.pop()
     print(f'{start_time_new} , {period_new}')
-    # combination_list = [start_time_combination]
-    # new_entry_list = [start_time_new]
+    combination_list = [start_time_combination]
+    new_entry_list = [start_time_new]
 
     while True:
-        if time_combination < start_time_new:
-            time_combination += period_combination
-        elif time_combination > start_time_new:
-            start_time_new += period_new
-        # if combination_list[-1] < new_entry_list[-1]:
-        #     combination_list.append(combination_list[-1] + period_combination)
-        # elif combination_list[-1] > new_entry_list[-1]:
-        #     new_entry_list.append(new_entry_list[-1] + period_new)
+        if len(combination_list) > 2 and len(new_entry_list) > 2:
+            break
+        if combination_list[-1] < new_entry_list[-1]:
+            combination_list.append(combination_list[-1] + period_combination)
+        elif combination_list[-1] > new_entry_list[-1]:
+            new_entry_list.append(new_entry_list[-1] + period_new)
         else:
             break
 
-    # # combine combination and new_entry in new values of start_time_combination, period_combination
+    # combine combination and new_entry in new values of start_time_combination, period_combination
     # start_time_combination = copy.deepcopy(combination_list[-1])
+    combo0 = combination_list.pop(0)
+    if new_entry_list[0] > combo0:
+        combo0 = combination_list.pop(0)
+    combo1 = combination_list.pop(0)
+
+    # find how many slots is the new_entry_list entry before combo0
+    # find how many slots is the new_entry_list entry before combo1
+    # use both slot counts above to calculate the new value of start_time_combination
+
     period_combination = period_new * period_combination
 
     # delete lists, as they won't be needed again
-    # del combination_list, new_entry_list
+    del combination_list, new_entry_list
 
-soln_B = period_combination - time_combination
+soln_B = period_combination - start_time_combination
 print(f'\nThe solution to Part B is {soln_B}\n')
