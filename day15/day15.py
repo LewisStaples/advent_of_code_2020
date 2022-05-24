4192540ec6d2104fed53e86ee481c8641ce52789
Lewis Staples <lewis.staples@gmail.com>
2022-05-24 16:43:52 -0400

    Day 15 parts A and B complete

--- day15/day15.py
+++ day15/day15.py
@@ -1,6 +1,9 @@
 # adventOfCode 2020 day 15
 # https://adventofcode.com/2020/day/15
 
+
+# last_number is the newest value of the speaking number
+# This function updates speakingNum_indices_dict to reflect the latest value of last_number
 def update_with_last_number():
     if last_number in speakingNum_indices_dict:
         speakingNum_indices_dict[last_number].append(turn_number)
@@ -13,35 +16,36 @@ def update_with_last_number():
 input_filename='input_sample0.txt'
 print(f'\nUsing input file: {input_filename}')
 with open(input_filename) as f:
-    # Pull in each line from the input file
-    for in_string in f:
-        in_string = in_string.rstrip()
-        print(f'With starting numbers: {in_string}')
+    in_string = f.readline().rstrip()
 
-    # Parse data from input file
-    starting_numbers = [int(num) for num in in_string.split(',')]
+print(f'With starting numbers: {in_string}')
 
-    speakingNum_indices_dict = {}
-    for turn_number, last_number in enumerate(starting_numbers, 1):
-        update_with_last_number()
+# Parse data from input file
+starting_numbers = [int(num) for num in in_string.split(',')]
+
+speakingNum_indices_dict = {}
+for turn_number, last_number in enumerate(starting_numbers, 1):
+    update_with_last_number()
+turn_number += 1
+
+del in_string
+del starting_numbers
+del input_filename
+
+# Loop turn for all calculated speaking numbers through end of program operation
+while True:
+    # "Speak" a number indicating how long ago it was previously spoken
+    if len(speakingNum_indices_dict[last_number]) == 1:
+        last_number = 0
+    else:
+        last_number = speakingNum_indices_dict[last_number][1] - speakingNum_indices_dict[last_number][0]
+    update_with_last_number()
+
+    if turn_number == 2020:
+        print(f'Part A result:  {turn_number}-th spoken is {last_number}')
+    if turn_number == 30000000:
+        print(f'Part B result:  {turn_number}-th spoken is {last_number}')
+        break
+
     turn_number += 1
 
-    del in_string
-    del starting_numbers
-    del input_filename
-
-    # Loop turn for all calculated speaking numbers through end of program operation
-    while True:
-        # "Speak" a number indicating how long ago it was previously spoken
-
-        if len(speakingNum_indices_dict[last_number]) == 1:
-            last_number = 0
-        else:
-            last_number = speakingNum_indices_dict[last_number][1] - speakingNum_indices_dict[last_number][0]
-        
-        update_with_last_number()
-        if turn_number == 2020:
-            print(f'Part A result:  {turn_number}-th spoken is {last_number}\n')
-            break
-        turn_number += 1
-dummy = 123
