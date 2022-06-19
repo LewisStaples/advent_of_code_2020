f0469f32ae0d85bc1dbfe3d3cb17531f1474a76d
Lewis Staples <lewis.staples@gmail.com>
2022-06-18 22:52:57 -0400

    Moved handleing of cards after a win to a function. Started logic to determine if recursion is called for

--- day22/day22_partB.py
+++ day22/day22_partB.py
@@ -21,6 +21,10 @@ class Game:
     def card_input(self, player_id, card):
         self._decks[player_id].append(card)
 
+    def record_win(self, winning_playerID):
+            self._decks[winning_playerID].append(self._player1card)
+            self._decks[winning_playerID].append(self._player2card)
+
     def do_round(self):
         self._round_counter += 1
         print(f'-- Round {self._round_counter} (Game {self._gameID}) --')
@@ -34,25 +38,33 @@ class Game:
         )
 
         # Each player draws a card
-        player1card = self._decks[1].pop(0)
-        player2card = self._decks[2].pop(0)
+        self._player1card = self._decks[1].pop(0)
+        self._player2card = self._decks[2].pop(0)
 
         # Verify if the newest deck state was seen before ("repeat")
         # (If there are not repeats the set's length will equal the round_number,
         # whereas the first repeat will cause the length to be less than the round_number)
         if len(self._set__decks_of_cards) < self._round_counter:
             # Player 1 wins
-            self._decks[1].append(player1card)
-            self._decks[1].append(player2card)
+            # self._decks[1].append(self._player1card)
+            # self._decks[1].append(self._player2card)
+            self.record_win(1)
             return
 
         # See if a recursive game should be played
+        if len(self._decks[1]) > self._decks[1][0]:
+            if len(self._decks[2]) > self._decks[2][0]:
+                # Create cursive object
+                dummy = 123 # fill this in !!!!!
 
         # If not using a recursive game, then play with the rules from part (a)
-        if player1card > player2card:
-            self._decks[1].append(player1card)
-            self._decks[2].append(player2card)
-            
+        if self._player1card > self._player2card:
+            self.record_win(1)
+        else:
+            self.record_win(2)
+            # self._decks[1].append(self._player1card)
+            # self._decks[1].append(self._player2card)
+
 game_list = [Game(None)]
 
 # Reading input from the input file
